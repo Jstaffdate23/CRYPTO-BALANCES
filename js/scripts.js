663fa01eea7da7f43460f8d16b45c2f14a3e0f6a
Jstaffdate23 <jhonjairogiron7805@hotmail.com>
2023-06-29 00:07:10 -0500

    export

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+import {getCategories} from "./Categorias.js" 
 // TODAY'S DATE
 
 var date = new Date().toLocaleDateString("en-GB", {
@@ -10,25 +11,7 @@ var date = new Date().toLocaleDateString("en-GB", {
 document.getElementById("date").innerHTML = date;
 //chart.js y Categorias
 
-const getCategories = async () => {
-  fetch("https://api.coingecko.com/api/v3/coins/categories/list", {
-    method: "GET",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("Categories:", data.length);
-      document.getElementById("categories").innerHTML = data.length;
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-};
+
 
 getCategories();
 
